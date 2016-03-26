0eb9b586290edcc432c3867b7c46cd4ae85bdb2b
Sonny Ambrogio <sonnyambrogio@gmail.com>
2016-03-26 10:05:37 -0400

    added Comment

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // Added a comment
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
